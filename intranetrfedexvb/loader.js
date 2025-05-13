312ad0b59a138bdf852111fb3b56bcda4a8dca49
Antonio Gat Fernández <antoniogatfernandez@iescastelar.com>
2025-05-13 16:29:42 +0200

    quitamos intranetfedexvb

--- intranetrfedexvb/loader.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-    const loader = document.querySelector('.loader');
-    loader.style.borderTop = '16px solid white';
-    loader.style.borderBottom = '16px solid gray';
-  
-    setTimeout(() => {
-      window.location.href = 'intranet.php';
-    }, 750); // Cambiar el tiempo según tus necesidades
-  }, 1000); // Cambiar el tiempo según tus necesidades 
